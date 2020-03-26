a4617a43047fe3c5de51a6db6b98349d9e6e8746
Cristian Jovanovich <cristian.jova149@gmail.com>
2020-03-26 07:44:06 -0300

    initial commit, init project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fileUpload = require('express-fileupload');
+
+// Init app
+const app = express();
+
+// Init fileupload
+app.use(fileUpload());
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// Homepage Route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Servicio iniciado en puerto ${PORT}`));
