8db9b8f8a6d1a55a85cb4d2e7bc76995d98f0053
Cristian Jovanovich <cristian.jova149@gmail.com>
2020-03-26 08:16:34 -0300

    file route finished

--- routes/api/file.js
+++ routes/api/file.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  console.log(req.files.upfile);
+  // Check if files is send
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file Uploaded' });
+  }
+
+  const file = req.files.upfile;
+
+  res.json({
+    fileName: file.name,
+    fileType: file.mimetype,
+    fileSize: file.size
+  });
 });
 
 module.exports = router;
